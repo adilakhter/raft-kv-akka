f35cbb754a33fb8b814892fddca40f28161a8168
ddworak <dawid.dworak@gmail.com>
2017-10-31 16:40:49 +0100

    Add message interceptor for testing.

--- src/multi-jvm/scala/pl/edu/agh/iosr/raft/BasicMultiNodeSpec.scala
+++ src/multi-jvm/scala/pl/edu/agh/iosr/raft/BasicMultiNodeSpec.scala
@@ -1,5 +1,6 @@
 package pl.edu.agh.iosr.raft
 
+import akka.actor.Actor
 import akka.remote.testkit.MultiNodeSpecCallbacks
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
 
@@ -9,4 +10,20 @@ trait BasicMultiNodeSpec extends MultiNodeSpecCallbacks
   override def beforeAll = multiNodeSpecBeforeAll()
 
   override def afterAll = multiNodeSpecAfterAll()
+}
+
+/**
+  * Use for testing.
+  *
+  * Forwards all messages received to the system's EventStream.
+  * Use this to deterministically `awaitForLeaderElection` etc.
+  */
+trait EventStreamAllMessages {
+  this: Actor =>
+
+  override def aroundReceive(receive: Actor.Receive, msg: Any) = {
+    context.system.eventStream.publish(msg.asInstanceOf[AnyRef])
+
+    receive.applyOrElse(msg, unhandled)
+  }
 }
