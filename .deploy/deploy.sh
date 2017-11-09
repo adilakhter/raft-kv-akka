36fc38b9e1c1650fd7f99cce4cf21618f5366aac
ddworak <dawid.dworak@gmail.com>
2017-11-09 21:03:05 +0100

    Fix argument order.

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -2,4 +2,4 @@
 chmod 600 .deploy/deploy.pem
 scp -o StrictHostKeyChecking=no -i .deploy/deploy.pem target/scala-2.12/raft.jar ubuntu@52.214.224.105:/home/ubuntu/
 ssh -o StrictHostKeyChecking=no -i .deploy/deploy.pem ubuntu@52.214.224.105 "killall -9 java; nohup java -jar /home/ubuntu/raft.jar `</dev/null` >app1.log 2>&1 &"
-ssh -o StrictHostKeyChecking=no -i .deploy/deploy.pem ubuntu@52.214.224.105 "killall -9 java; nohup java -jar /home/ubuntu/raft.jar -Dclustering.port=2552 worker`</dev/null` >app2.log 2>&1 &"
+ssh -o StrictHostKeyChecking=no -i .deploy/deploy.pem ubuntu@52.214.224.105 "killall -9 java; nohup java -Dclustering.port=2552 -jar /home/ubuntu/raft.jar worker`</dev/null` >app2.log 2>&1 &"
